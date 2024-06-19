510f40f5d9c42eeeab18e2ba26fdaea97c07735e
Eric Fernandes <comun.ucc2.uniteltmais@hotmail.com>
2024-06-19 19:37:51 +0200

    create Test Error_Name_Empty

--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
 using CommonTestsUtilities.Cryptography;
 using CommonTestsUtilities.Mapper;
 using CommonTestsUtilities.Repositories;
@@ -21,6 +23,21 @@ public class RegisterUserUseCaseTest
         result.Should().NotBeNull();
         result.Name.Should().Be(request.Name);
         result.Token.Should().NotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var usecase = CreateUseCase();
+
+        var act = async () => await usecase.Execute(request);
+
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+
+        result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.NAME_EMPTY));
     }
 
     private RegisterUserUseCase CreateUseCase()
