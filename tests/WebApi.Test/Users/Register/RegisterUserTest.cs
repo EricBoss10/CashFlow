b42c6d9020150bceb765438f1198113cd16fa800
Eric Fernandes <comun.ucc2.uniteltmais@hotmail.com>
2024-07-04 18:30:07 +0200

    create test integration of Error Empty Name

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,8 +1,10 @@
-﻿using CommonTestsUtilities.Requests;
+﻿using CashFlow.Exception;
+using CommonTestsUtilities.Requests;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace WebApi.Test.Users.Register;
 
@@ -25,5 +27,31 @@ public class RegisterUserTest: IClassFixture<CustomWebApplicationFactory>
         var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
         result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
+        response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Error_Empty_Name()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessage").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(ResourceErrorMessages.NAME_EMPTY));
     }
 }
