bc6aefec67113a522fb22bc3c52a179b49b07947
Eric Fernandes <comun.ucc2.uniteltmais@hotmail.com>
2024-07-09 20:38:35 +0200

    Integration Test Login

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,6 @@
-﻿using CashFlow.infrastructure.DataAccess;
+﻿using CashFlow.Domain.Security.Cryptography;
+using CashFlow.infrastructure.DataAccess;
+using CommonTestsUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +10,9 @@ namespace WebApi.Test;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private CashFlow.Domain.Entities.User _user;
+    private string _password;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -20,6 +25,29 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     config.UseInMemoryDatabase("InMemoryDbForTesting");
                     config.UseInternalServiceProvider(provider);
                 });
+
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbcontext =  scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+                var passwordEncripter =  scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+
+                StartDatabase(dbcontext, passwordEncripter);
             });
+    }
+
+    public string GetEmail() => _user.Email;
+
+    public string GetName() => _user.Name;
+
+    public string GetPassword() => _password;
+    private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.Password;
+
+        _user.Password = passwordEncripter.Encrypt(_user.Password);
+
+        dbContext.Users.Add(_user);
+
+        dbContext.SaveChanges();
     }
 }
