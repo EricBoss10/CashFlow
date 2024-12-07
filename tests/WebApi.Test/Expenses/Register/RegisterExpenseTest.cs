f26e37a661a04f3593eca3b2bd0fa6d1741482bd
Eric Fernandes <comun.ucc2.uniteltmais@hotmail.com>
2024-12-07 15:38:22 +0100

    Register expense Test

---
+++ tests/WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
@@ -0,0 +1,37 @@
+﻿using CommonTestsUtilities.Requests;
+using System.Net.Http.Json;
+using WebApi.Test.InlineData;
+using FluentAssertions;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Test.Expenses.Register;
+
+public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string METHOD = "api/Expenses";
+
+    private readonly HttpClient _httpClient;
+
+    public RegisterExpenseTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestExpenseJsonBuilder.Build();
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("title").GetString().Should().Be(request.Title);
+    }
+}
